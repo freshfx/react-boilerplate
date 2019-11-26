9430238f58a2b3880f6e77e7a925f69558c2692a
Sebastian Slamanig <sebastian@freshfx.at>
2019-11-26 11:55:14 +0100

    style(listitem): updated decleration; added props destructuring;

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types'
 import Item from './Item'
 import Wrapper from './Wrapper'
 
-function ListItem(props) {
-  return (
-    <Wrapper>
-      <Item>{props.item}</Item>
-    </Wrapper>
-  )
-}
+const ListItem = ({item}) => (
+  <Wrapper>
+    <Item>{item}</Item>
+  </Wrapper>
+)
 
 ListItem.propTypes = {
   item: PropTypes.any
