8a6f1aad86238d3f406a1a662586e0d233c928c4
Sebastian Slamanig <sebastian@freshfx.at>
2019-11-26 11:59:09 +0100

    refactor(toggle): refactored component rendering

--- app/components/Toggle/index.js
+++ app/components/Toggle/index.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import PropTypes from 'prop-types'
 import noop from 'lodash/noop'
 
 import Select from './Select'
 import ToggleOption from '../ToggleOption'
 
-function Toggle(props) {
-  let content = <option>--</option>
+const Toggle = props => {
+  const renderOption = value =>
+    <ToggleOption key={value} value={value} message={props.messages[value]} />
 
-  // If we have items, render them
-  if (props.values.length) {
-    content = props.values.map(value =>
-      <ToggleOption key={value} value={value} message={props.messages[value]} />)
-  }
+  const content = useMemo(() => {
+    // If we have items, render them
+    if (props.values.length) {
+      return props.values.map(renderOption)
+    }
+    return <option>--</option>
+  }, [props.messages, props.values])
 
   return (
     <Select value={props.value} onChange={props.onToggle}>
