9ba272e47cb41168bf5152058efe41d11eb17280
Sebastian Slamanig <sebastian@freshfx.at>
2019-11-26 11:19:13 +0100

    refactor(notfoundpage): refactored to FC

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -10,12 +10,12 @@ import {FormattedMessage} from 'react-intl'
 import H1 from 'components/H1'
 import messages from './messages'
 
-export default function NotFound() {
-  return (
-    <article>
-      <H1>
-        <FormattedMessage {...messages.header} />
-      </H1>
-    </article>
-  )
-}
+const NotFoundPage = () => (
+  <article>
+    <H1>
+      <FormattedMessage {...messages.header} />
+    </H1>
+  </article>
+)
+
+export default React.memo(NotFoundPage)
