87726aac2ec03a6242b01795c15d2dcbd7997427
Sebastian Slamanig <sebastian@freshfx.at>
2021-02-12 12:33:16 +0100

    feat(page-loading-indicator): added page loading indicator for loadable pages

---
+++ app/components/molecules/PageLoadingIndicator/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import LoadingIndicator from 'components/atoms/LoadingIndicator'
+
+import Holder from './Holder'
+
+const PageLoadingIndicator = () => (
+  <Holder>
+    <LoadingIndicator />
+  </Holder>
+)
+
+export default PageLoadingIndicator
